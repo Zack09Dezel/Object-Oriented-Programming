947972306c613b4567ee69a66dc89ead5a05e540
Zack09Dezel <edo09ozymandias@gmail.com>
2024-09-17 02:35:09 +0800

    More clarification and future plans

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,20 +1,21 @@
 import javax.swing.JOptionPane;
-
+//! Need to beautify the UI for fancier navigation.
 public class Calculator {
     public static void main(String[] args) {
 
         String option = JOptionPane.showInputDialog("What do you want to do? \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Divition");
         double x = Double.parseDouble(JOptionPane.showInputDialog("First number: ")), y = Double.parseDouble(JOptionPane.showInputDialog("Second Number: ")), result = 0;
         Operations Addition = new Operations(), Subtraction = new Operations(), Multiplication = new Operations(), Divition = new Operations();
-
+        
+        //TODO: Need to find a way to integrate N variable into the operations, possible through Object Overload.
         switch (option) {
             case "1":
             case "Addition":
                 result = Addition.Addition(x, y);
                 JOptionPane.showMessageDialog(null, "The result is: "+result);
                 break;
-            case "Subtraction:":
             case "2":
+            case "Subtraction:":
                 result = Subtraction.Subtraction(x, y);
                 JOptionPane.showMessageDialog(null, "The result is: "+result);
                 break;
--- Calculator/src/Operations.java
+++ Calculator/src/Operations.java
@@ -1,4 +1,5 @@
 public class Operations {
+    //! Need to learn object overload.
     public double Addition(double x, double y){
         return x + y;
     }
@@ -14,4 +15,7 @@ public class Operations {
     public double Divition(double x, double y){
         return x / y;
     }
+
+    // TODO: Add more functions like Modulo, Power(N), Square Root, Log, Ln, Absolute, Trigonometry, etc.
+
 }
