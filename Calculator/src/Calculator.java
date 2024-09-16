e857896cb74f03aa1380d950221f45bbc5bc4650
Zack09Dezel <edo09ozymandias@gmail.com>
2024-09-17 02:17:23 +0800

    Operations classes added for clarity

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -5,49 +5,33 @@ public class Calculator {
 
         String option = JOptionPane.showInputDialog("What do you want to do? \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Divition");
         double x = Double.parseDouble(JOptionPane.showInputDialog("First number: ")), y = Double.parseDouble(JOptionPane.showInputDialog("Second Number: ")), result = 0;
+        Operations Addition = new Operations(), Subtraction = new Operations(), Multiplication = new Operations(), Divition = new Operations();
 
         switch (option) {
             case "1":
             case "Addition":
-                result = Addition(x, y);
+                result = Addition.Addition(x, y);
                 JOptionPane.showMessageDialog(null, "The result is: "+result);
                 break;
             case "Subtraction:":
             case "2":
-                result = Subtraction(x, y);
+                result = Subtraction.Subtraction(x, y);
                 JOptionPane.showMessageDialog(null, "The result is: "+result);
                 break;
             case "3":
             case "Multiplication":
-                result = Multiplication(x, y);
+                result = Multiplication.Multiplication(x, y);
                 JOptionPane.showMessageDialog(null, "The result is: "+result);
                 break;
             case "4":
             case "Divition":
-                result = Divition(x, y);
+                result = Divition.Divition(x, y);
                 JOptionPane.showMessageDialog(null, "The result is: "+result);
                 break;  
             default:
                 break;
         }
 
-    }
-
-    //*Operations
-    public static double Addition(double x, double y){
-        return x + y;
-    }
-
-    public static double Subtraction(double x, double y){
-        return x - y;
-    }
-
-    public static double Multiplication(double x, double y){
-    return x * y;
-    }
-
-    public static double Divition(double x, double y){
-        return x / y;
     }
 
 }
---
+++ Calculator/src/Operations.java
@@ -0,0 +1,17 @@
+public class Operations {
+    public double Addition(double x, double y){
+        return x + y;
+    }
+
+    public double Subtraction(double x, double y){
+        return x - y;
+    }
+
+    public double Multiplication(double x, double y){
+    return x * y;
+    }
+
+    public double Divition(double x, double y){
+        return x / y;
+    }
+}
