7274c510f1e2a8fdd81100a0a1fa135a8599a57a
Zack09Dezel <edo09ozymandias@gmail.com>
2024-09-19 15:34:48 +0800

    Final?

--- Personal Biodata/src/Main.java
+++ Personal Biodata/src/Main.java
@@ -4,25 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Biodata person = new Biodata();
-        
-        System.out.println("What is your name, how old are you, and what is your Student ID number?");
+
+        System.out.print("What is your name? ");
         String name = input.nextLine();
+        System.out.print("How old are you? ");
         int age = input.nextInt();
+        input.nextLine();
+        System.out.print("What is you student ID number? ");
         long nim = input.nextLong();
         input.nextLine();  
-
-        System.out.println("How many are your hobbies?");
+        System.out.print("How many are your hobbies? ");
         int totalHobbies = input.nextInt();
         input.nextLine();
 
         String[] hobbies = new String[totalHobbies];
         for(int i = 0; i < totalHobbies;i++){
-            System.out.println("Hobby number "+(i + 1)+": ");
+            System.out.print("Hobby number "+(i + 1)+": ");
             hobbies[i] = input.nextLine();
         }
 
         person.scan(name, age, nim, hobbies);
-
         input.close();
         person.print();
     }
